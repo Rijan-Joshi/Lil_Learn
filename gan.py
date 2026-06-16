2af9df26880dee28b8f1daa5f1ca9af8fad4c6e8
Rijan Shrestha <csaijspy@gmail.com>
2026-06-17 00:30:05 +0545

    feat: load MNIST dataseat

---
+++ gan.py
@@ -0,0 +1,44 @@
+# Imports
+
+from tqdm import tqdm
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+import torch
+import torchsummary
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+
+import torchvision
+from torchvision import datasets, transforms
+
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print("The device used is " + device)
+
+torch.manual_seed(100)
+np.random.seed(100)
+
+
+# Loading the MNIST datasets
+
+transform = transforms.Compose(
+    [
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081)),
+        transforms.Lambda(lambda x: x.view(-1)),
+    ]
+)
+
+train_data = datasets.MNIST(
+    root="./data", train=False, download=True, transform=transform
+)
+
+test_data = datasets.MNIST(
+    root="./data", train=False, download=True, transform=transform
+)
+
+train_loader = DataLoader(train_data, batch_size=64, shuffle=True)
+test_loader = DataLoader(test_data, batch_size=64, shuffle=True)
